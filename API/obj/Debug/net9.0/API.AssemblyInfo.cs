6459bc77ccc90d6acd196af49d9605108a82bd93
ChienDainn <chien.minh001.com>
2025-04-28 20:50:25 +0700

    End section

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+304682401fdd3c488ce9c5dd526c2dd33e261f53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecdbc159087b3e4d4885d479f74bf1959d302e7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
