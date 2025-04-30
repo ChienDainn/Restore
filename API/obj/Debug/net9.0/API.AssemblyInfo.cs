3d5d194d1d3f8d794c15a3636268662c5e8aef3f
ChienDainn <chien.minh001.com>
2025-04-30 14:59:36 +0700

    Modify product controller

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69fa9b88f56b23d8088cc207ca4d2dd3df7abb98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89fc237da4a74604d43850dcde32b1d1f139bad8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
